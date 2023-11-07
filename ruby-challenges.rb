2afe146c364a21a92d14c6e2cccf473a28d6cda9
Joseph Kong <josephk1018@gmail.com>
2023-11-06 21:08:38 -0800

    week-4-assessment-completed

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -11,6 +11,24 @@ reposts3 = 221
 # Expected output: '221 is odd'
 
 # Pseudo code:
+# input: an integer
+# out put: a string that tells whether the integer is even or odd
+# create a method that takes in an integer
+# write an conditional statement using unless 
+# unless the condition .even? is false, the output is always "#{integer} is odd"
+# use else to return the other scenerio when the condition is true and the integer is even
+
+def even_or_odd (integer)
+    unless integer.even? 
+        "#{integer} is odd"
+    else 
+        "#{integer} is even"
+    end
+end
+p even_or_odd (reposts1)
+p even_or_odd (reposts2)
+p even_or_odd (reposts3)
+
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
@@ -23,6 +41,19 @@ beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
 # Pseudo code:
+# input: a string
+# output: a string that contains only the consenant from the original strings but not vowels. 
+# create a function that takes in a string
+# use .delete to remove "AEIOUaeiou"
+
+def consonance (string)
+    string.delete "AEIOUaeiou"
+end
+p consonance(beatles_album1)
+p consonance(beatles_album2)
+p consonance(beatles_album3)
+
+
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
 
@@ -34,3 +65,21 @@ palindrome_test_case3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
 
 # Pseudo code:
+# input: a string 
+# output: a message/string that tells wheter the string passed in is a palindorm or not
+# create a method that takes in a string 
+# set up a condtional statement 
+# use .reverse and boolean evaluation to see if the the string in reverse order is the same as it is before. if yes -->the "#{string} is a palindrome"
+# else print out a message that "#{string} is not a palindrome"
+
+def palindrome (string)
+    if string.downcase == string.downcase.reverse
+        "#{string} is a palindrome"
+    else 
+        "#{string} is not a palindrome"
+    end
+end
+
+p palindrome(palindrome_test_case1)
+p palindrome(palindrome_test_case2)
+p palindrome(palindrome_test_case3)
